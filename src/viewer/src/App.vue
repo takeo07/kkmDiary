cb75d1e1eae8868d379e62e85aee8f52589e7008
sabitetsu <miya.takeo@gmail.com>
2025-07-21 20:28:25 +0900

    add src folder and src/viewer folder.

---
+++ src/viewer/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <VCalendar />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import { VCalendar } from 'vuetify/labs/VCalendar';
+
+</script>
