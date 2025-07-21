6bdd61f39a4b9bb8c0e4195dd717dd9f38166c07
sabitetsu <miya.takeo@gmail.com>
2025-07-21 20:55:06 +0900

    add example for calendar

--- src/viewer/src/App.vue
+++ src/viewer/src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <v-app>
     <v-main>
-      <VCalendar />
+      <VCalendar :events="events"/>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  import { VCalendar } from 'vuetify/labs/VCalendar';
+import { ref } from 'vue';
+import { VCalendar } from 'vuetify/labs/VCalendar';
 
+const events = ref([
+  {
+    title: 'Meeting with Team',
+    start: new Date('2025-07-22'),
+    end: new Date('2025-07-22'),
+    color: 'blue',
+  },
+  {
+    title: 'Project Deadline',
+    start: new Date('2025-07-25'),
+    end: new Date('2025-07-25'),
+    color: 'red',
+  },
+  {
+    title: 'Conference',
+    start: new Date('2025-07-28'),
+    end: new Date('2025-07-29'),
+    color: 'green',
+  },
+]);
 </script>
